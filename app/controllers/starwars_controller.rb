a387467008b8a69ca96425ad2be352d30dc3ca95
nmhummel <natalie.hummel@gmail.com>
2020-12-09 14:37:34 -0600

    all aspects seem to be working.

--- app/controllers/starwars_controller.rb
+++ app/controllers/starwars_controller.rb
@@ -1,12 +1,12 @@
 class StarwarsController < ApplicationController
 
-# GET: /stories - get or see all starwars
-  get "/starwars" do
+  # GET: /stories - get or see all starwars
+  get "/starwars" do # request info to see and display it
+    redirect_if_not_logged_in
     @starwars = Starwar.all
     erb :"/starwars/index" 
   end
-  # request info to see and display it
-
+  
   # GET: /starwars/new - get the form to create a new starwar (does not create)
   get "/starwars/new" do 
     redirect_if_not_logged_in
@@ -19,13 +19,14 @@ class StarwarsController < ApplicationController
     starwar = Starwar.new(params)
     starwar.user_id = session[:user_id]
     # not a @var because we're going to redirect and lose this data anyway
+    #faker
+    binding.pry
     starwar.save 
     #@@madlibs << starwar
     redirect "/starwars/#{starwar.id}" 
     # makes a new GET request - sending info to server to do something with it
   end
   
-
   # GET: /starwars/5 - get one specific starwar
   get "/starwars/:id" do 
     @starwar = Starwar.find(params[:id]) #or "id"
@@ -33,24 +34,25 @@ class StarwarsController < ApplicationController
     erb :"/starwars/show"
   end
 
-  # GET: /starwars/5/edit - get the form to edit a specific movie...
+  # GET: /starwars/5/edit - get the form to edit a specific starwar...
   get "/starwars/:id/edit" do 
     @starwar = Starwar.find(params["id"]) 
     # the above line of code should be in every route that has :id
     redirect_if_invalid 
-    end
     erb :"/starwars/edit"
   end
 
-  # PATCH: /starwars/5 - ...and update that specific movie using patch/put
+  # PATCH: /starwars/5 - ...and update that specific starwar using patch/put
   patch "/starwars/:id" do # put/patch
     redirect_if_not_logged_in
     @starwar = Starwar.find(params["id"])
     redirect_if_invalid
     #@starwar.update[title:]
     #@starwar.update(title: params["starwar"]["title"])
-    @movie.update(params["movie"])
-    redirect "/starwars/#{@movie.id}"
+    #binding.pry
+    @starwar.update(params["starwars"]) # ???
+    #faker
+    redirect "/starwars/#{@starwar.id}"
   end
 
   # DELETE: /starwars/5/delete - destroy a starwar from the database
@@ -63,10 +65,19 @@ class StarwarsController < ApplicationController
   end
 
   private
+
   def redirect_if_invalid
-    if @movie.user_id != session[:user_id]
-      redirect:"/starwars"  
+    if @starwar.user_id != session[:user_id]
+      redirect "/starwars"  
     end
   end
+
+  # def faker
+  #   params.each do |key, value|
+  #     if value("planet_1").empty?
+  #       value = Faker::Movies::StarWars.planet
+  #     end
+  #   end
+  # end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,44 +1,79 @@
 class UsersController < ApplicationController
 # users need to be able to sign up
 
-get "/signup" do
-  #shows form to sign up
-  erb :"/users/new"
-end
-
-post "/signup" do
-  #handles signing up the user
-  user = User.new(params) #instantiate a user
-  # make sure user signs up with valid data
-  if 
-    params.values.all?{|value| value.blank?} || User.find_by_email(params[:email])
-    #user.email.blank? || user.password.blank? || User.find_by_email(params[:email])
-    redirect "/signup"
-  else
-    # log them in
-    user.save
-    session[:user_id] = user.id # this line of code actually logs us in
-    redirect "/starwars"
+  get "/users" do
+    @starwars = current_user.starwars
+    erb :"/users/index"
   end
-end
 
-get "/login" do # to show the form
-  erb :"/users/login"
-end
-
-post "/login" do # process the form
-  user = User.find_by_username(params[:username])
-  if user && user.authenticate(params[:password]) # if user found and authenticated
-    session[:user_id] = user.id
-    redirect "/starwars"
-  else
-    redirect "/login"
+  get "/signup" do
+    #shows form to sign up
+    erb :"/users/new"
   end
+
+  post "/signup" do
+    #handles signing up the user
+    redirect_if_not_logged_in # ???
+    user = User.new(params) #instantiate a user
+    # make sure user signs up with valid data
+    if 
+      params.values.all?{|value| value.blank?} || User.find_by_email(params[:email])
+      #user.email.blank? || user.password.blank? || User.find_by_email(params[:email])
+      redirect "/signup"
+    else
+      # log them in
+      user.save
+      session[:user_id] = user.id # this line of code actually logs us in
+      redirect "/users"
+    end
+  end
+
+  get "/login" do # to show the form
+    @starwars = Starwar.all
+    erb :"/users/login"
+  end
+
+  post "/login" do # process the form
+    @starwars = Starwar.all
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password]) # if user found and authenticated
+      session[:user_id] = user.id
+      redirect "/users"
+    else
+      redirect "/login"
+    end
+  end
+
+  get "/logout" do
+    session.clear
+    #session.delete(:user_id) # delete just the user_id
+    redirect "/"
+  end
+
 end
 
-get "/logout" do
-  session.clear
-  #session.delete(:user_id) # delete just the user_id
-  redirect "/signup"
-end
 
+  # get "/users/:id" do
+  #   user = User.find_by_username(params[:username])
+  #   erb :"/show"
+  # end
+
+  
+  #### which do we need? this or the previous one? or both?
+  # get "/users/index" do
+  #   redirect_if_not_logged_in
+  #   @starwars = Starwar.all
+  #   @user = User.find(params["id"]) 
+  #   @stories = Starwar.find_by(params["user_id"]) # added 
+
+    
+  #   #@users = User.all
+  #   @user_by_id = User.find(params["id"])
+  #   @user_stories = Starwar.find_by(params["user_id"])
+
+  #   @user_story = []
+  #   @starwars.each do |s|
+  #     @user_story << s.user_id
+  #   end
+  #   erb :"/users/index"
+  # end
