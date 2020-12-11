8de5c6852a8f3023cd0d724aa3e647ab9119f8ec
nmhummel <natalie.hummel@gmail.com>
2020-12-10 21:55:29 -0600

    fixed some styling and added 'created by username' to pages

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
     has_secure_password # ActiveRecord method (predefined)
     # provides a reader/writer method for password
     validates :username, presence: true, uniqueness: { message: "%{value} is already in use.  Please select another or login."}
-    validates :email, presence: true, uniqueness: { message: "%{value} is already in use.  Please select another or login."}
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }, presence: true, uniqueness: { message: "%{value} is already in use.  Please select another or login."}
 end
