f0ac878f186818c6bad4743c37a5b35426256176
nmhummel <natalie.hummel@gmail.com>
2020-12-21 14:09:00 -0600

    environment.rb changes

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-require 'dotenv/load'
+Dotenv.load if ENV['SINATRA_ENV'] == "development"
 
 # ActiveRecord::Base.establish_connection(
 #   :adapter => "sqlite3",
@@ -10,8 +10,6 @@ require 'dotenv/load'
 # )
 
 set :database_file, "./database.yml"
-
-Dotenv.load if ENV['SINATRA_ENV'] == "development"
 
 require './app/controllers/application_controller'
 require_all 'app'
